4395847785d5d1daf3b86450313855f0e4248d27
terachan3700 <chapico01@gmail.com>
2018-11-12 21:58:32 +0900

    セッターメソッドは明示的にレシーバが必要となるサンプルを作成

--- chapter2/09.rb
+++ chapter2/09.rb
@@ -17,3 +17,21 @@ x = SetMe.new
 x.value = 1
 
 puts x.value
+
+class Counter
+  attr_accessor(:counter)
+
+  def initialize
+    # counter = 0      #<- selfをつけない場合、スコープが終わるとcounterを捨ててまう。
+    self.counter = 0
+    # @counter = 0     #<- インスタンス変数でもよい
+  end
+
+  def counter_next
+    counter + 1        #<- こっちはselfいらない。セッターでないから
+  end
+end
+
+con = Counter.new
+puts con.counter
+puts con.counter_next

a5e4239040b0272c8b9b2d8423627c496b08c22f
terachan3700 <chapico01@gmail.com>
2018-11-13 21:55:29 +0900

    meanメソッド追加。これだとどのhashキーが使えるか調べないと行けない

--- chapter2/10.rb
+++ chapter2/10.rb
@@ -6,19 +6,29 @@ class AnnualWeather
 
     CSV.foreach(file_name, headers: true) do |row|
       @readings << {
-        data: Date.parse(row[2]),
+        date: Date.parse(row[2]),
         high: row[10].to_f,
         low: row[11].to_f
       }
-
     end
   end
 
   def readings
     @readings
   end
+
+  def mean
+    return 0.0 if @readings.size.zero?
+
+    total = @readings.reduce(0.0) do |sum, reading|
+      sum + (reading[:high] + reading[:low]) / 2.0
+    end
+
+    total / @readings.size.to_f
+  end
 end
 
 an = AnnualWeather.new("../data/weather.csv")
 
 puts an.readings
+puts an.mean

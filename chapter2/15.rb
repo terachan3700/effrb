d01a90fc97308e85382c56641d0c609a415d5fbf
terachan3700 <chapico01@gmail.com>
2018-11-15 22:28:50 +0900

    クラス変数だと継承先でも共有されるため、インスタンス変数に変更

--- chapter2/15.rb
+++ chapter2/15.rb
@@ -1,7 +1,7 @@
 class Singleton
   private_class_method(:new, :dup, :clone)
   def self.instance
-    @@single ||= new
+    @single ||= new
   end
 end
 
@@ -10,3 +10,14 @@ sin2 = Singleton.instance
 
 puts sin.object_id
 puts sin2.object_id
+
+class Configuration < Singleton
+end
+
+class Database < Singleton
+end
+
+# 継承しても、同じオブジェクトになる。
+# スーパークラスのクラス変数はそのクラスと全てのサブクラスの間で共有される。
+puts Configuration.instance.object_id
+puts Database.instance.object_id

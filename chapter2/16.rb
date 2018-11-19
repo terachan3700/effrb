cd4ef32307337879b2e2dc1a2161e6019e46c97c
terachan3700 <chapico01@gmail.com>
2018-11-19 21:24:13 +0900

    参照渡しのサンプル

---
+++ chapter2/16.rb
@@ -0,0 +1,23 @@
+class Tuner
+  def initialize(presets)
+    @presets = presets
+    clean
+  end
+
+  private
+
+  def clean
+    @presets.delete_if { |f| f[-1].to_i.even? }
+  end
+end
+
+presets = %w(90.1 106.2 88.5)
+
+puts presets
+
+# 参照渡しなので、presetsの値が消えてしまう。
+tuner = Tuner.new(presets)
+
+puts tuner
+
+puts presets

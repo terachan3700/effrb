3ec3962ae603489f2e57859a5502fdcac98cb5dc
terachan3700 <chapico01@gmail.com>
2018-11-19 22:41:31 +0900

    第一弾にhashを使ってパフォーマンスをあげる

--- chapter2/18.rb
+++ chapter2/18.rb
@@ -1,9 +1,17 @@
 class Role
   def initialize(name, permissions)
-    @name, @permissions = name, permissions
+    @name = name
+    @permissions = Hash[permissions.map { |p| [p, true] }]
   end
 
   def can?(permission)
     @permissions.include?(permission)
   end
 end
+
+
+role = Role.new("terada",%w(a b c))
+puts role
+
+puts role.can?('e')
+
